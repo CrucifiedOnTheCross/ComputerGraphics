588a9844aa44bc663c94832db2541b0ef8449659
Silver <toooriiiiuuuuum@gmail.com>
2024-03-05 11:50:40 +0400

    Star (2.0) update - add img - add ObjModel - refactor folder

---
+++ lwOne/objectreader.py
@@ -0,0 +1,20 @@
+class ObjModel:
+    def __init__(self):
+        self.vertex = []
+        self.polygons = []
+
+    def readModel(self, file: str):
+        try:
+            with open(file, 'r') as obj:
+                for line in obj:
+                    if line.startswith('v '):
+                        x, y, z = map(float, line.split()[1:])
+                        self.vertex.append((x, y, z))
+                    elif line.startswith('f '):
+                        vertices = [int(v.split('/')[0]) for v in line.split()[1:]]
+                        self.polygons.append(vertices)
+        except FileNotFoundError:
+            print(f"File '{file}' not found.")
+
+    def getVertexFromPolygon(self, polygon):
+        return [self.vertex[v - 1] for v in polygon]
