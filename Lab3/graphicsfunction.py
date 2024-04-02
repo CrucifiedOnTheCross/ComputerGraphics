6fa745ce02b82262ea11cc444d92e00c7a90eb7b
SystemSl <144921270+SystemSl@users.noreply.github.com>
2024-04-02 14:42:03 +0400

    Add files via upload

--- Lab3/graphicsfunction.py
+++ Lab3/graphicsfunction.py
@@ -10,28 +10,27 @@ def getBarCor(x, y, x0, y0, x1, y1, x2, y2):
 
 
 def _movPol(ax, ay, pol, width, height):
-    # return [[(ax * i[0]) / i[2] + width, (ay * i[1]) / i[2] + height] for i in pol]
-    return [[i[0] + width, i[1] + height - 500] for i in pol]
+    return [[ax * i[0] / i[2] + width, ay * i[1] / i[2] + height - 200] for i in pol]
 
 
 def drawTriangle(img, pol, ax, ay, color, zBuffer):
     movPol = _movPol(ax, ay, pol, len(img) / 2, len(img[0]) / 2)
     xmin = max(0, min(movPol[0][0], movPol[1][0], movPol[2][0]))
     ymin = max(0, min(movPol[0][1], movPol[1][1], movPol[2][1]))
-    xmax = min(len(img), max(movPol[0][0], movPol[1][0], movPol[2][0]))
-    ymax = min(len(img[0]), max(movPol[0][1], movPol[1][1], movPol[2][1]))
+    xmax = min(len(img) - 2, max(movPol[0][0], movPol[1][0], movPol[2][0]))
+    ymax = min(len(img[0]) - 2, max(movPol[0][1], movPol[1][1], movPol[2][1]))
 
-    for x in range(int(xmin), min(int(xmax) + 1, len(img))):
-        for y in range(int(ymin), min(int(ymax) + 1, len(img[0]))):
+    zBuf = 0
+    for x in range(int(xmin), int(xmax) + 1):
+        for y in range(int(ymin), int(ymax) + 1):
             barCor = getBarCor(x, y, movPol[0][0], movPol[0][1], movPol[1][0], movPol[1][1], movPol[2][0], movPol[2][1])
-            zBuf = barCor[0] * (pol[0][2]+10000) + barCor[1] * (pol[1][2]+10000) + barCor[2] * (pol[2][2]+10000)
+            zBuf = barCor[0] * pol[0][2] + barCor[1] * pol[1][2] + barCor[2] * pol[2][2]
             if all(i > 0 for i in barCor) and zBuffer[x][y] > zBuf:
                 zBuffer[x][y] = zBuf
                 img[x][y] = color
+
 
 def getNormal(pol):
     v1 = np.array([pol[1][0] - pol[0][0], pol[1][1] - pol[0][1], pol[1][2] - pol[0][2]])
     v2 = np.array([pol[2][0] - pol[1][0], pol[2][1] - pol[1][1], pol[2][2] - pol[1][2]])
     return np.cross(v1, v2)
-
-# %%
