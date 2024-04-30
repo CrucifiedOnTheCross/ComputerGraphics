338a6705deb4ebf3785826c6e68c109a9ce4739f
SystemSl <144921270+SystemSl@users.noreply.github.com>
2024-04-30 14:48:05 +0400

    Last task
    Texturing with Guro shadow

--- Lab3/graphicsfunction.py
+++ Lab3/graphicsfunction.py
@@ -11,23 +11,40 @@ def getBarCor(x, y, x0, y0, x1, y1, x2, y2):
 
 def _movPol(ax, ay, pol, width, height):
     # return [[(ax * i[0]) / i[2] + width, (ay * i[1]) / i[2] + height] for i in pol]
-    return [[i[0] + width, i[1] + height - 500] for i in pol]
+    return [[i[0], i[1]] for i in pol]
 
 
-def drawTriangle(img, pol, ax, ay, color, zBuffer):
-    movPol = _movPol(ax, ay, pol, len(img) / 2, len(img[0]) / 2)
+def drawTriangleShadow(img, pol, ax, ay, DEFAULT_COLOR, I0, I1, I2, zBuffer):
+    movPol = pol
     xmin = max(0, min(movPol[0][0], movPol[1][0], movPol[2][0]))
     ymin = max(0, min(movPol[0][1], movPol[1][1], movPol[2][1]))
-    xmax = min(len(img), max(movPol[0][0], movPol[1][0], movPol[2][0]))
-    ymax = min(len(img[0]), max(movPol[0][1], movPol[1][1], movPol[2][1]))
+    xmax = min(len(img), max(movPol[0][0], movPol[1][0], movPol[2][0]) + 1)
+    ymax = min(len(img[0]), max(movPol[0][1], movPol[1][1], movPol[2][1]) + 1)
 
     for x in range(int(xmin), min(int(xmax) + 1, len(img))):
         for y in range(int(ymin), min(int(ymax) + 1, len(img[0]))):
             barCor = getBarCor(x, y, movPol[0][0], movPol[0][1], movPol[1][0], movPol[1][1], movPol[2][0], movPol[2][1])
-            zBuf = barCor[0] * (pol[0][2]+10000) + barCor[1] * (pol[1][2]+10000) + barCor[2] * (pol[2][2]+10000)
-            if all(i > 0 for i in barCor) and zBuffer[x][y] > zBuf:
+            zBuf = barCor[0] * (pol[0][2]) + barCor[1] * (pol[1][2]) + barCor[2] * (pol[2][2])
+            if all(i >= 0 for i in barCor) and zBuffer[x][y] > zBuf:
                 zBuffer[x][y] = zBuf
-                img[x][y] = color
+                img[x][y] = [max(-255*(barCor[0]*I0 + barCor[1]*I1 + barCor[2]*I2), 0),
+                             max(-255*(barCor[0]*I0 + barCor[1]*I1 + barCor[2]*I2), 0),
+                             max(-255*(barCor[0]*I0 + barCor[1]*I1 + barCor[2]*I2), 0)]
+
+def drawTriangleTexture(img, pol, ax, ay, texture_array, texture, pol_texture, zBuffer):
+    movPol = pol
+    xmin = max(0, min(movPol[0][0], movPol[1][0], movPol[2][0]))
+    ymin = max(0, min(movPol[0][1], movPol[1][1], movPol[2][1]))
+    xmax = min(len(img), max(movPol[0][0], movPol[1][0], movPol[2][0]) + 1)
+    ymax = min(len(img[0]), max(movPol[0][1], movPol[1][1], movPol[2][1]) + 1)
+
+    for x in range(int(xmin), min(int(xmax) + 1, len(img))):
+        for y in range(int(ymin), min(int(ymax) + 1, len(img[0]))):
+            barCor = getBarCor(x, y, movPol[0][0], movPol[0][1], movPol[1][0], movPol[1][1], movPol[2][0], movPol[2][1])
+            zBuf = barCor[0] * (pol[0][2]) + barCor[1] * (pol[1][2]) + barCor[2] * (pol[2][2])
+            if all(i >= 0 for i in barCor) and zBuffer[x][y] > zBuf:
+                zBuffer[x][y] = zBuf
+                img[x][y] = texture_array[int(len(texture_array[0])*(barCor[0]*texture[pol_texture[0] - 1][1] + barCor[1]*texture[pol_texture[1] - 1][1] + barCor[2]*texture[pol_texture[2] - 1][1]))][int(len(texture_array)*(barCor[0]*texture[pol_texture[0] - 1][0] + barCor[1]*texture[pol_texture[1] - 1][0] + barCor[2]*texture[pol_texture[2] - 1][0]))]
 
 def getNormal(pol):
     v1 = np.array([pol[1][0] - pol[0][0], pol[1][1] - pol[0][1], pol[1][2] - pol[0][2]])
--- Lab3/objectreader.py
+++ Lab3/objectreader.py
@@ -5,6 +5,12 @@ class ObjModel:
     def __init__(self):
         self.vertex = []
         self.polygons = []
+        self.texture = []
+        self.normal = []
+        self.polygons_texture = []
+        
+        textureFlag = False
+        normalFlag = False
 
         self.minx = float('inf')
         self.miny = float('inf')
@@ -25,10 +31,29 @@ class ObjModel:
                         self.miny = min(self.miny, y)
                         self.maxx = max(self.maxx, x)
                         self.maxy = max(self.maxy, y)
-                        self.minz = max(self.maxx, z)
+                        self.minz = min(self.maxx, z)
                         self.maxz = max(self.maxy, z)
                     elif line.startswith('f '):
-                        self.polygons.append([int(v.split('/')[0]) for v in line.split()[1:]])
+                        pol = []
+                        pol_t = []
+                        pol_n = []
+                        for v in line.split()[1:]:
+                            ar = v.split('/') 
+                            pol.append(int(ar[0]))
+                            pol_t.append(int(ar[1]))
+                            pol_n.append(int(ar[2]))
+                        for i in range(1, len(pol)-1):
+                            self.polygons.append([pol[0], pol[i], pol[i+1]])
+                            self.polygons_texture.append([pol_t[0], pol_t[i], pol_t[i+1]])
+                            #self.polygons_normal.append([pol_n[0], pol_n[i], pol_n[i+1]])
+                        
+                    elif line.startswith('vt '):
+                        x, y = map(float, line.split()[1:3])
+                        self.texture.append([x, y])
+                        
+                    elif line.startswith('vn '):
+                        x, y, z = map(float, line.split()[1:])
+                        self.normal.append([x, y, z])
 
         except FileNotFoundError:
             print(f"File '{file}' not found.")
@@ -55,21 +80,31 @@ class ObjModel:
         zangle_rad = np.radians(zangle)
 
         rotation_x = np.array([[1, 0, 0],
-                               [0, np.cos(xangle_rad), -np.sin(xangle_rad)],
-                               [0, np.sin(xangle_rad), np.cos(xangle_rad)]])
+                               [0, np.cos(xangle_rad), np.sin(xangle_rad)],
+                               [0, -np.sin(xangle_rad), np.cos(xangle_rad)]])
 
-        rotation_y = np.array([[np.cos(yangle_rad), 0, np.sin(yangle_rad)],dd
+        rotation_y = np.array([[np.cos(yangle_rad), 0, np.sin(yangle_rad)],
                                [0, 1, 0],
                                [-np.sin(yangle_rad), 0, np.cos(yangle_rad)]])
 
-        rotation_z = np.array([[np.cos(zangle_rad), -np.sin(zangle_rad), 0],
-                               [np.sin(zangle_rad), np.cos(zangle_rad), 0],
+        rotation_z = np.array([[np.cos(zangle_rad), np.sin(zangle_rad), 0],
+                               [-np.sin(zangle_rad), np.cos(zangle_rad), 0],
                                [0, 0, 1]])
 
         rotation_matrix = np.dot(rotation_x, np.dot(rotation_y, rotation_z))
 
         for i in range(len(self.vertex)):
             self.vertex[i] = np.dot(rotation_matrix, self.vertex[i])
+            self.normal[i] = np.dot(rotation_matrix, self.normal[i])
+
+        self.minx = min([v[0] for v in self.vertex])
+        self.miny = min([v[1] for v in self.vertex])
+        self.maxx = max([v[0] for v in self.vertex])
+        self.maxy = max([v[1] for v in self.vertex])
+        self.minz = min([v[2] for v in self.vertex])
+        self.maxz = max([v[2] for v in self.vertex])
+
+        
 
     def moveObj(self, moveVector):
         for i in range(len(self.vertex)):
